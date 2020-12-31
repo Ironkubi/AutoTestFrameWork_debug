71aa518ef246b926ab27a5e8fff17435ab4ea270
Ironkubi <419406847@qq.com>
2020-12-31 18:03:48 +0800

    update Allure

--- debug_2.py
+++ debug_2.py
@@ -1,1 +1,32 @@
+import time
+import pytest
 
+
+@pytest.fixture(scope="module", autouse=True)
+def start(request):
+    print('\n----开始执行module----')
+    print("module: %s \n----回到首页---" % request.module.__name__)
+    print("----启动浏览器----")
+    yield
+    print("-----结束测试，end-----")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def open_home(request):
+    print("function: %s \n----回到首页---" % request.function.__name__)
+
+
+def test_01():
+    print("----用例01-----")
+
+
+def test_02():
+    print("----用例02-----")
+
+
+def test_03():
+    print("----用例03-----")
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", "test_AutoIsTrue.py"])
